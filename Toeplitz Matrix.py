81c5dd64cbd6e38f922e466da12c9b38a1c1231f
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-18 20:00:12 +0300

    Task 9

--- Toeplitz Matrix.py
+++ Toeplitz Matrix.py
@@ -15,6 +15,7 @@ class Solution(object):
                             pass
                         else:
                             y = False
+                            
         return y
              
                 
