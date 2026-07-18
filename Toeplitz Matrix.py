1efce61fed92ad035614fdc13f3895ba948f2622
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-18 18:37:04 +0300

    Task7

---
+++ Toeplitz Matrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        y = True
+        for i in range(len(matrix)):
+            if i>0:
+                x = matrix[i]
+                z = matrix[i-1]
+                for j in range(len(matrix[i])):
+                    if j>0:
+                        if x[j] == z[j-1]:
+                            pass
+                        else:
+                            y = False
+        return y
+             
+                
+
+        
